b530a4860276e2082b930b09e271807ef14a5413
Rogant <rogant@gmail.com>
2016-06-21 15:01:26 -0500

    step-0

---
+++ app/app.ts
@@ -0,0 +1,22 @@
+import {Component} from '@angular/core';
+import {Platform, ionicBootstrap} from 'ionic-angular';
+import {StatusBar} from 'ionic-native';
+
+
+@Component({
+  template: '<ion-nav [root]="rootPage"></ion-nav>'
+})
+export class MyApp {
+
+  private rootPage:any;
+
+  constructor(private platform:Platform) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+    });
+  }
+}
+
+ionicBootstrap(MyApp)
