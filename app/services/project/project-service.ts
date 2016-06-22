a51d88eb1b3275c1640b8f36bc23ff5ba4903bea
Rogant <rogant@gmail.com>
2016-06-22 11:45:21 -0500

    step-6

---
+++ app/services/project/project-service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Headers, RequestOptions } from '@angular/http';
+
+import { Project }           from './project';
+import { Observable }     from 'rxjs/Observable';
+
+@Injectable()
+export class ProjectService {
+  constructor(private _Http: Http) {}
+
+  private ProjectApi = 'http://192.168.60.65:9000/api/projects';  // URL to web API
+
+  addProject(name: string): Observable<Project> {
+    let body = JSON.stringify({ name });
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+
+    return this._Http.post(this.ProjectApi, body, options)
+      .map(this.extractData)
+      .catch(this.handleError);
+  }
+  private extractData(res: Response) {
+    let body = res.json();
+    return body.data || {};
+  }
+  private handleError(error: any) {
+    // In a real world app, we might use a remote logging infrastructure
+    // We'd also dig deeper into the error to get a better message
+    let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg); // log to console instead
+    return Observable.throw(errMsg);
+  }
+}
